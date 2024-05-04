e7958363d209a9d4beaf01f60f4013456e643f85
caiohenry <caio.feitosa09@aluno.ifce.edu.br>
2024-05-04 01:05:28 -0300

    Iniciação do projeto Pesquisa-Plus

---
+++ pesquisa_plus/src/main/java/br/com/pesquisa_plus/pesquisa_plus/PesquisaPlusApplication.java
@@ -0,0 +1,13 @@
+package br.com.pesquisa_plus.pesquisa_plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesquisaPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesquisaPlusApplication.class, args);
+	}
+
+}
---
+++ pesquisa_plus/src/test/java/br/com/pesquisa_plus/pesquisa_plus/PesquisaPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pesquisa_plus.pesquisa_plus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesquisaPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
