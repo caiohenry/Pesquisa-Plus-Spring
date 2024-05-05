d3dbd4ecc85a4a4d0476e09ed35a48697d6a980d
Caio Henrique <caio.feitosa09@aluno.ifce.edu.br>
2024-05-05 05:21:01 -0300

    Ajustes no ambiente de produção do sistema

--- pesquisa_plus/src/main/java/br/com/pesquisa_plus/pesquisa_plus/PesquisaPlusApplication.java
+++ pesquisa_plus/src/main/java/br/com/pesquisa_plus/pesquisa_plus/PesquisaPlusApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PesquisaPlusApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		SpringApplication.run(PesquisaPlusApplication.class, args);
 	}
 
